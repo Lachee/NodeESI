f5a53a6f24071c2415ef39413205b45f5a0874f5
Lachee <lachlan.h@hotmail.com>
2022-11-26 00:58:18 +1100

    Revert "Replaced with axios adapters"
    This reverts commit c0084c882bed13ba6ef8c7c2126cd9b0fab45d40.

--- src/adapter.js
+++ src/adapter.js
@@ -1,12 +1,11 @@
 const instance = require("./index");
 const crypto = require("crypto");
 const axios = require("axios");
-const httpAdapter = require('axios/lib/adapters/http');
 const sleep = require("util").promisify(setTimeout);
 
 /** Adapter for regular requests */
 async function EsiAdapter(request) {
-    request.adapter = httpAdapter;
+    request.adapter = axios.default.adapter;
     return await makeRequest(request);
 }
 
@@ -21,7 +20,7 @@ async function CacheAdapter(request) {
         return cache.response;
     }
 
-    request.adapter = httpAdapter;
+    request.adapter = axios.default.adapter;
     const response = await makeRequest(request);
     if (response.headers.expires) {
         cache.cacheResponse(response);
@@ -53,7 +52,7 @@ async function ETagCacheAdapter(request) {
         }
     }
 
-    request.adapter = httpAdapter;
+    request.adapter = axios.default.adapter;
     if (etagObject != null)
         request.headers['If-None-Match'] = etagObject.etag;
 
